063973fc3c773b5548235d58480f16ef98f1cfbf
shihabsaleem <shihabchikkushihab@gmail.com>
2023-10-28 21:01:08 +0530

    commit

--- searchapp/views.py
+++ searchapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from ecom.models import Product, Category
+from django.db.models import Q
+
 
 # Create your views here.
+def searchResult(request):
+    products = None
+    query = None
+    if 'q' is request.GET:
+        query = request.GET.get('q')
+        products = Product.objects.all().filter(Q(name__contains=query) | (Q(desc__contains=query)))
+        return render(request, 'search.html', {'query': query, 'products': products})
