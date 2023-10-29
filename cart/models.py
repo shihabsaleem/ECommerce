ba5b4d436b5d025e4396cf83d71e4b09de2dd550
shihabsaleem <shihabchikkushihab@gmail.com>
2023-10-29 17:31:44 +0530

    cart

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from ecom.models import Product  # Import the Product model from the 'ecom' app
+
 
 # Create your models here.
+
+class Cart(models.Model):
+    cart_id = models.CharField(max_length=200, blank=True)  # A unique identifier for the cart
+    date_added = models.DateField(auto_now_add=True)  # Date when the cart was created
+
+    class Meta:
+        db_table = 'cart'  # Set the database table name to 'cart'
+        ordering = ['date_added']  # Define the default ordering based on the date_added field
+
+    def __str__(self):
+        return self.cart_id  # Return a string representation of the cart (its ID)
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)  # A link to the product being added to the cart
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)  # A link to the cart to which the product is added
+    quantity = models.IntegerField()  # The quantity of the product in the cart
+    active = models.BooleanField(default=True)  # Indicates whether the cart item is active
+
+    class Meta:
+        db_table = 'CartItem'  # Set the database table name to 'CartItem'
+
+    def sub_total(self):
+        return self.product.price * self.quantity  # Calculate the subtotal for the cart item
+
+    def __str__(self):
+        return str(self.product)  # Return a string representation of the cart item (the product's name)
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from ecom.models import Product
+from .models import Cart, CartItem
+from django.core.exceptions import ObjectDoesNotExist
+
 
 # Create your views here.
+
+def _cart_id(request):
+    # Generate a unique cart ID for the current session
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+
+def add_cart(request, product_id):
+    # Add a product to the shopping cart
+    product = Product.objects.get(id=product_id)
+
+    try:
+        # Try to retrieve an existing cart
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+    except Cart.DoesNotExist:
+        # If no cart exists, create a new one
+        cart = Cart.objects.create(cart_id=_cart_id(request))
+        cart.save()
+
+    try:
+        # Try to retrieve an existing cart item for the product
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        # If no cart item exists, create a new one
+        cart_item = CartItem.objects.create(product=product, quantity=1, cart=cart)
+        cart_item.save()
+
+    return redirect('cart:cart_detail')  # Redirect to the cart detail page after adding the product
+
+
+def cart_detail(request, total=0, counter=0, cart_items=None):
+    try:
+        # Try to retrieve the cart based on the cart ID
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, active=True)
+
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            counter += cart_item.quantity
+    except ObjectDoesNotExist:
+        pass
+
+    return render(request, 'cart.html', {'cart_items': cart_items, 'total': total, 'counter': counter})
